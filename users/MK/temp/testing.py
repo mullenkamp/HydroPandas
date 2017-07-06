1b99f92fc601a9750ecb28950a9367328f59dd17
Mike Exner-Kittridge <Mike.Exner-Kittridge@ecan.govt.nz>
2017-07-06 20:07:22 +1200

    testing

--- users/MK/temp/testing.py
+++ users/MK/temp/testing.py
@@ -2470,8 +2470,10 @@ sites_xa = sites_df1.to_xarray()
 from bokeh.plotting import figure, output_file, show
 from os.path import join
 
-base_dir = r'E:\ecan\local\Projects\plotting\bokeh'
+base_dir = r'S:\Surface Water\backups\MichaelE\Projects\plotting\bokeh'
 html1 = 'lines.html'
+html2 = 'log_lines.html'
+html3 = 'color_scatter.html'
 
 # prepare some data
 x = [1, 2, 3, 4, 5]
@@ -2490,6 +2492,60 @@ p.line(x, y, legend="Temp.", line_width=2)
 show(p)
 
 
+# prepare some data
+x = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+y0 = [i**2 for i in x]
+y1 = [10**i for i in x]
+y2 = [10**(i**2) for i in x]
+
+# output to static HTML file
+output_file(join(base_dir, html2))
+
+# create a new plot
+p = figure(
+   tools="pan,box_zoom,reset,save",
+   y_axis_type="log", y_range=[0.001, 10**11], title="log axis example",
+   x_axis_label='sections', y_axis_label='particles'
+)
+
+# add some renderers
+p.line(x, x, legend="y=x")
+p.circle(x, x, legend="y=x", fill_color="white", size=8)
+p.line(x, y0, legend="y=x^2", line_width=3)
+p.line(x, y1, legend="y=10^x", line_color="red")
+p.circle(x, y1, legend="y=10^x", fill_color="red", line_color="red", size=6)
+p.line(x, y2, legend="y=10^x^2", line_color="orange", line_dash="4 4")
+
+# show the results
+show(p)
+
+
+import numpy as np
+
+from bokeh.plotting import figure, output_file, show
+
+# prepare some data
+N = 4000
+x = np.random.random(size=N) * 100
+y = np.random.random(size=N) * 100
+radii = np.random.random(size=N) * 1.5
+colors = [
+    "#%02x%02x%02x" % (int(r), int(g), 150) for r, g in zip(50+2*x, 30+2*y)
+]
+
+# output to static HTML file (with CDN resources)
+output_file(join(base_dir, html3), title="color_scatter.py example", mode="cdn")
+
+TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
+
+# create a new plot with the tools above, and explicit ranges
+p = figure(tools=TOOLS, x_range=(0,100), y_range=(0,100))
+
+# add a circle renderer with vectorized colors and sizes
+p.circle(x,y, radius=radii, fill_color=colors, fill_alpha=0.6, line_color=None)
+
+# show the results
+show(p)
 
 
 
