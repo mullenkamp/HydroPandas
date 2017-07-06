e8a09c2f3bb5d5e750de78d711751229242ce9f1
MikeK <mgkittridge@gmail.com>
2017-07-07 09:36:27 +1200

    added

--- users/MK/met/metservice/metservice_proc_nc.py
+++ users/MK/met/metservice/metservice_proc_nc.py
@@ -31,3 +31,5 @@ new_nc = proc_metservice_nc(nc)
 precip, sites = MetS_nc_to_df(new_nc)
 
 new_precip = sel_interp_agg(precip, sites.crs, poly, grid_res, data_col, time_col, x_col, y_col, buffer_dis, agg_xy=True)
+
+
