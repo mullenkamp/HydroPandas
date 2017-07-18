7c86bc04224b9931ece978cd8f8c52d9e601ca75
MikeK <mgkittridge@gmail.com>
2017-07-18 16:24:47 +1200

    initial water report py

---
+++ users/MK/hydro/monthly_water_report/water_report1.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 16:27:09 2017
+
+@author: MichaelEK
+"""
+
+from geopandas import read_file
+from core.classes.hydro import hydro, all_mtypes
+from datetime import datetime
+from pandas import DateOffset, to_datetime, date_range
+
+###################################################
+#### Parameters
+
+sw_poly_shp = r'P:\Surface Water Quantity\Projects\Freshwater Report\Water_Report_layers.shp'
+swl_points_shp = r'P:\Surface Water Quantity\Projects\Freshwater Report\WL_recorders.shp'
+
+site_col = 'SITENUMBER'
+
+qual_codes = [10, 18, 20, 30, 50]
+
+##################################################
+#### Read in data
+
+sw_poly = read_file(sw_poly_shp)
+swl_points = read_file(swl_points_shp)
+
+flow1 = hydro().get_data('flow', swl_points[site_col], qual_codes)
+precip = hydro().get_data(mtypes='precip', sites=sw_poly_shp, qual_codes=qual_codes)
+
+stats_flow = flow1.stats('flow')
+stats_precip = precip.stats('precip')
+
+### Select precip sites with long-ish record
+latest1 = stats_precip['End time'].max()
+past1 = latest1 - DateOffset(months=6)
+all_dates = date_range(start=past1, end=latest1)
+
+precip_sites = stats_precip[(stats_precip['Tot data yrs'] >= 10) & stats_precip['End time'].isin(all_dates)]
+
+precip1 = precip.sel('precip', sites=precip_sites.index)
+
+
+##################################################
+####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
