da88a1aeed82ab01685fe2c4dbc4d096c47b930f
MattH <hansonmcoombs@gmail.com>
2017-09-14 14:01:51 +1200

    debug data at wells

--- users/MH/Waimak_modeling/models/extended_boundry/model_runs/model_run_tools/data_extraction/data_at_wells.py
+++ users/MH/Waimak_modeling/models/extended_boundry/model_runs/model_run_tools/data_extraction/data_at_wells.py
@@ -12,10 +12,12 @@ import flopy
 import os
 from warnings import warn
 from copy import deepcopy
-from users.MH.Waimak_modeling.models.extended_boundry.supporting_data_analysis.all_well_layer_col_row import get_all_well_row_col
+from users.MH.Waimak_modeling.models.extended_boundry.supporting_data_analysis.all_well_layer_col_row import \
+    get_all_well_row_col
 
-hds_no_data = -999.99 #todo check with BRIOCH this has changed
-unc_no_data = -999.99 #todo confirm this
+hds_no_data = -999  # todo check with BRIOCH this has changed
+unc_no_data = -999.99  # todo confirm this
+
 
 def get_well_positions(well_list, missing_handeling='warn'):
     """
@@ -29,7 +31,7 @@ def get_well_positions(well_list, missing_handeling='warn'):
 
     well_list = np.atleast_1d(well_list)
     all_wells = get_all_well_row_col()
-    t = set(well_list) - set(all_wells)
+    t = set(well_list) - set(all_wells.index)
     if len(t) > 0:
         if missing_handeling == 'raise':
             raise ValueError('missing wells from list {}'.format(t))
@@ -37,24 +39,29 @@ def get_well_positions(well_list, missing_handeling='warn'):
             pass
         elif missing_handeling == 'warn':
             warn('missing wells from list {}'.format(t))
-    all_wells.rename(columns={'layer': 'k', 'row': 'i', 'col': 'j'})
-    outdata = all_wells.loc[well_list,['k', 'i', 'j', 'nztmx', 'nztmy', 'depth', 'mid_screen_elv']]
-    missing = outdata.loc[:,['k', 'i', 'j']].isnull().values.sum()
-    if missing > 0:
+    all_wells = all_wells.rename(columns={'layer': 'k', 'row': 'i', 'col': 'j'})
+    outdata = all_wells.loc[well_list, ['k', 'i', 'j', 'nztmx', 'nztmy', 'depth', 'mid_screen_elv']]
+    missing = (outdata.k.isnull() | outdata.i.isnull() | outdata.j.isnull())
+    if missing.sum() > 0:
         if missing_handeling == 'raise':
             raise ValueError('returned {} wells, missing i, j, or k'.format(missing))
         elif missing_handeling == 'forgive':
-            pass
+            outdata = outdata.dropna(subset=['k', 'i', 'j'])
         elif missing_handeling == 'warn':
-            warn('returned  {} wells, missing i, j, or k'.format(missing))
-    # todo check this
+            warn('returned  {} wells, missing i, j, or k'.format(outdata.loc[missing].index))
+            outdata = outdata.dropna(subset=['k', 'i', 'j'])
     return outdata
+
 
 def get_hds_file_path(name_file_path=None, hds_path=None, m=None):
     loc_inputs = 0
     if hds_path is not None:
+        if '.hds' not in hds_path:
+            raise ValueError('expected .hds in hds path: {}'.format(hds_path))
         loc_inputs += 1
     if name_file_path is not None:
+        if '.nam' not in name_file_path:
+            raise ValueError('expected .nam in name file path: {}'.format(name_file_path))
         loc_inputs += 1
         with open(name_file_path) as f:
             name_file_data = pd.Series(f.readlines())
@@ -65,14 +72,14 @@ def get_hds_file_path(name_file_path=None, hds_path=None, m=None):
         temp2 = temp2.loc[temp2.str.contains('.hds')]
         if len(temp2) != 1:
             raise ValueError('could not identify heads file from name_file')
-        hds_path = '{}/{}'.format(os.path.dirname(name_file_path),temp2.iloc[0])
+        hds_path = '{}/{}'.format(os.path.dirname(name_file_path), temp2.iloc[0])
     if m is not None:
         loc_inputs += 1
-        temp = np.array(m.output_fnames)
+        temp = pd.Series(m.output_fnames)
         temp2 = temp.loc[temp.str.contains('.hds')]
         if len(temp2) != 1:
             raise ValueError('could not identify heads file from model')
-        hds_path = '{}/{}'.format(m.model_ws,temp2.iloc[0])
+        hds_path = '{}/{}'.format(m.model_ws, temp2.iloc[0])
 
     if loc_inputs == 0:
         raise ValueError('must define one of: name_file_path, hds_path, m')
@@ -80,7 +87,9 @@ def get_hds_file_path(name_file_path=None, hds_path=None, m=None):
         raise ValueError('must define only one of: name_file_path, hds_path, m')
     return hds_path
 
-def get_hds_at_wells(well_list, kstpkpers=None, rel_kstpkpers=None, name_file_path=None, hds_path=None, m=None, add_loc=False):
+
+def get_hds_at_wells(well_list, kstpkpers=None, rel_kstpkpers=None, name_file_path=None, hds_path=None, m=None,
+                     add_loc=False, missing_handling='warn'):
     """
     return dataframe of heads at wells in well list
     :param well_list: list of well numbers to export data at
@@ -91,6 +100,9 @@ def get_hds_at_wells(well_list, kstpkpers=None, rel_kstpkpers=None, name_file_pa
     :param hds_path:  None or the path to the heads file, must define one of name_file_path, hds_path, m
     :param m: None or a flopy model object, must define one of name_file_path, hds_path, m
     :param add_loc: boolean if true add k,i,j, x,y,z to outdata
+    :param missing_handeling: one of: 'raise' : raise an exception for any nan values with well numbers
+                              'forgive': silently remove nan values
+                              'warn': remove nan values, but issue a warning with well numbers
     :return: df of heads (rows: wells  columns:kstpkpers)
     """
     # set up inputs and outdata
@@ -99,17 +111,15 @@ def get_hds_at_wells(well_list, kstpkpers=None, rel_kstpkpers=None, name_file_pa
 
     kstpkpers = _get_kstkpers(hds_file, kstpkpers, rel_kstpkpers)
 
-    kstpkper_names = ['{}_{}'.format(e[0],e[1]) for e in kstpkpers]
-    well_locs = get_well_positions(well_list)
-    outdata = pd.DataFrame(index=well_list,columns=kstpkper_names)
+    kstpkper_names = ['hd_m3d_kstp{}_kper{}'.format(e[0], e[1]) for e in kstpkpers]
+    well_locs = get_well_positions(well_list, missing_handeling=missing_handling)
+    outdata = pd.DataFrame(index=well_list, columns=kstpkper_names)
     if add_loc:
-        outdata = pd.merge(outdata,well_locs,right_index=True,left_index=True)
-
-    hds = hds_file.get_alldata(nodata=hds_no_data)
+        outdata = pd.merge(outdata, well_locs, right_index=True, left_index=True)
 
     outdata = _fill_df_with_bindata(hds_file, kstpkpers, kstpkper_names, outdata, hds_no_data, well_locs)
-    #todo check this
     return outdata
+
 
 def get_con_at_wells(well_list, unc_file_path, kstpkpers=None, rel_kstpkpers=None, add_loc=False):
     """
@@ -125,42 +135,48 @@ def get_con_at_wells(well_list, unc_file_path, kstpkpers=None, rel_kstpkpers=Non
     unc_file = flopy.utils.UcnFile(unc_file_path)
 
     kstpkpers = _get_kstkpers(unc_file, kstpkpers, rel_kstpkpers)
-    kstpkper_names = ['{}_{}'.format(e[0],e[1]) for e in kstpkpers]
+    kstpkper_names = ['con_gm3_kstp{}_kper{}'.format(e[0], e[1]) for e in kstpkpers]
     well_locs = get_well_positions(well_list)
     outdata = pd.DataFrame(index=well_list, columns=kstpkper_names)
     if add_loc:
-        outdata = pd.merge(outdata,well_locs,right_index=True,left_index=True)
+        outdata = pd.merge(outdata, well_locs, right_index=True, left_index=True)
 
     outdata = _fill_df_with_bindata(unc_file, kstpkpers, kstpkper_names, outdata, unc_no_data, well_locs)
-    #todo check this
     return outdata
 
-def _get_kstkpers(bud_file, kstpkpers, rel_kstpkpers):
+
+def _get_kstkpers(bud_file, kstpkpers=None, rel_kstpkpers=None):
     if kstpkpers is not None and rel_kstpkpers is not None:
         raise ValueError('must define only one of kstpkpers or rel_kstpkpers')
     elif kstpkpers is not None:
         kstpkpers = np.atleast_2d(kstpkpers)
         if kstpkpers.shape[1] != 2:
             raise ValueError('must define both kstp and kper in kstpkpers')
+        check = [(e[0], e[1]) not in bud_file.get_kstpkper() for e in kstpkpers]
+        if any(check):
+            raise ValueError('{} kstpkpers not in bud_file'.format(kstpkpers[check]))
     elif rel_kstpkpers is not None:
         if rel_kstpkpers == 'all':
             kstpkpers = np.atleast_2d(bud_file.get_kstpkper())
         else:
             rel_kstpkpers = np.atleast_1d(rel_kstpkpers)
             if rel_kstpkpers.ndim != 1:
-                raise ValueError ('too many dimensions for rel_kstpkpers')
+                raise ValueError('too many dimensions for rel_kstpkpers')
             temp = bud_file.get_kstpkper()
-            kstpkpers = np.atleast_2d([temp[e] for e in rel_kstpkpers])
+            try:
+                kstpkpers = np.atleast_2d([temp[e] for e in rel_kstpkpers])
+            except IndexError as e_val:
+                raise IndexError(e_val.message + ' only {} kstpkpers in bud_file'.format(len(bud_file.get_kstpkper())))
     elif kstpkpers is None and rel_kstpkpers is None:
         raise ValueError('must define one of kstpkpers or rel_kstpkpers')
-
     return kstpkpers
 
-def _fill_df_with_bindata(bin_file,kstpkpers,kstpkper_names,df,nodata_value,locations):
+
+def _fill_df_with_bindata(bin_file, kstpkpers, kstpkper_names, df, nodata_value, locations):
     df = deepcopy(df)
     data = bin_file.get_alldata(nodata=nodata_value)
     mkstpkper = bin_file.get_kstpkper()
-    for kstpkper, name in zip(kstpkpers,kstpkper_names):
+    for kstpkper, name in zip(kstpkpers, kstpkper_names):
         kstpkper = tuple(kstpkper)
         temp = [e == kstpkper for e in mkstpkper]
 
@@ -168,17 +184,24 @@ def _fill_df_with_bindata(bin_file,kstpkpers,kstpkper_names,df,nodata_value,loca
             raise ValueError('{} kstpkper not in model kstpkpers'.format(kstpkper))
         temp2 = np.where(temp)
 
-        if len(temp2 != 1) or len(temp2[0]) !=1:
+        if len(temp2) != 1 or len(temp2[0]) != 1:
             raise ValueError('indexing error getting kstpkper location')
         idx = temp2[0][0]
 
-        time = np.ones((len(locations.index),))*idx
-        k = locations.k.values
-        i = locations.i.values
-        j = locations.j.values
+        time = (np.ones((len(locations.index),)) * idx).astype(int)
+        k = locations.k.values.astype(int)
+        i = locations.i.values.astype(int)
+        j = locations.j.values.astype(int)
         locs = zip(time, k, i, j)
-        df.loc[:, name] = data[locs] #todo check this thouroughly in a multi dimensional space
+        df.loc[:, name] = data[time, k, i, j]
 
         return df
 
+#todo with first real use of this spot check spatially, but should be right
 
+if __name__ == '__main__':
+    hds = (
+    r"C:\Users\MattH\Desktop\Waimak_modeling\python_models\component_con_n_ss\rch_constant_con\mf_constant_concentration.hds")
+    unc =r"C:\Users\MattH\Desktop\Waimak_modeling\python_models\component_con_n_ss\rch_constant_con\MT3D001.UCN"
+    test = get_con_at_wells(well_list=['M35/1003', 'M35/6295','BS28/5004'], unc_file_path=unc, rel_kstpkpers='all', add_loc=True)
+    print(test)
