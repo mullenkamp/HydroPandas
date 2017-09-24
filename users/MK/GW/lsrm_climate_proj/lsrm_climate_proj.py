21c9e761af7a95167c4f6ecb7add8f754379e6d4
Mike Exner-Kittridge <Mike.Exner-Kittridge@ecan.govt.nz>
2017-09-25 09:29:44 +1300

    update

--- users/MK/GW/lsrm_climate_proj/lsrm_climate_proj.py
+++ users/MK/GW/lsrm_climate_proj/lsrm_climate_proj.py
@@ -78,7 +78,7 @@ all_dir.extend(rcp8_dir)
 all_dir.extend(rcppast_dir)
 
 base_param_dict = {'no_irr': {'irr_eff': 1, 'include_irr': False}, '65perc': {'irr_eff': 0.65, 'include_irr': True}, '100perc': {'irr_eff': 1, 'include_irr': True}}
-vcsn_param_dict = {'50perc': {'irr_eff': 0.5, 'include_irr': True}, '65perc': {'irr_eff': 0.65, 'include_irr': True}, '80perc': {'irr_eff': 0.8, 'include_irr': True}, '100perc': {'irr_eff': 1, 'include_irr': True}}
+vcsn_param_dict = {'50perc': {'irr_eff': 0.5, 'include_irr': True}, '65perc': {'irr_eff': 0.65, 'include_irr': True}, '80perc': {'irr_eff': 0.8, 'include_irr': True}, '100perc': {'irr_eff': 1, 'include_irr': True}, 'no_irr': {'irr_eff': 1, 'include_irr': False}}
 
 param_dict = OrderedDict()
 for f in all_dir:
