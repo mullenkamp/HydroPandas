9ff3f685fe658213665614d2b3e876fadc261a18
matth <hansonmcoombs@gmail.com>
2017-09-27 13:17:43 +1300

    try to fix memory error

--- users/MH/Waimak_modeling/models/extended_boundry/model_runs/model_run_tools/model_bc_data/rch_support/generate_an_mean_rch.py
+++ users/MH/Waimak_modeling/models/extended_boundry/model_runs/model_run_tools/model_bc_data/rch_support/generate_an_mean_rch.py
@@ -15,7 +15,7 @@ test_path = r"K:\niwa_netcdf\lsrm\lsrm_results\vcsn_65perc.h5"
 
 
 def gen_water_year_average_lsr_irr(path):
-    org_data = pd.read_hdf(path)
+    org_data = pd.read_hdf(path).drop(['x', 'y','irr_eff', 'irr_trig','non_irr_aet', 'irr_aet','irr_paw', 'w_irr'], axis=1)
     if 'irr_drainage' not in org_data.keys():
         org_data.loc[:, 'irr_drainage'] = 0
     if 'irr_demand' not in org_data.keys():
@@ -25,21 +25,21 @@ def gen_water_year_average_lsr_irr(path):
 
     org_data.loc[org_data.irr_drainage.isnull(), 'irr_drainage'] = 0
     org_data.loc[org_data.non_irr_drainage.isnull(), 'non_irr_drainage'] = 0
-    org_data.loc[:, 'total_drainage'] = org_data.loc[:, 'irr_drainage'] + org_data.loc[:, 'non_irr_drainage']
+    org_data['total_drainage'] = org_data.irr_drainage + org_data.non_irr_drainage
     g = org_data.groupby([pd.Grouper('site'), pd.Grouper(key='time', freq='A-JUN')])
     temp = org_data.drop_duplicates('site').set_index('site')
     temp.loc[temp.irr_area_ratio.isnull(), 'irr_area_ratio'] = 0
     outdata = g.aggregate(
         {'precip': np.nansum, 'pet': np.nansum, 'paw': np.nansum, 'irr_drainage': np.nansum, 'irr_demand': np.nansum,
          'non_irr_drainage': np.nansum, 'total_drainage': np.nansum}).reset_index()
-    outdata.loc[:, 'site_area'] = outdata.loc[:, 'site']
-    outdata.loc[:, 'irr_area_ratio'] = outdata.loc[:, 'site']
+    outdata['site_area'] = outdata.loc[:, 'site']
+    outdata['irr_area_ratio'] = outdata.loc[:, 'site']
     outdata = outdata.replace({'site_area': temp.site_area.to_dict()})
     outdata = outdata.replace({'irr_area_ratio': temp.irr_area_ratio.to_dict()})
     for key in ['precip', 'pet', 'paw', 'total_drainage']:  # raising memory error so trying to do it one at a time
         outdata[key] *= (1 / (365 * outdata.site_area))
-    outdata['irr_drainage'] = (1 / (365 * outdata.site_area * outdata.irr_area_ratio))  # per area of irrigation
-    outdata['irr_demand'] = (1 / (365))  # m3/day
+    outdata['irr_drainage'] *= (1 / (365 * outdata.site_area * outdata.irr_area_ratio))  # per area of irrigation
+    outdata['irr_demand'] *= (1 / (365))  # m3/day
     outdata['non_irr_drainage'] = (
     1 / (365 * outdata.site_area * (1 - outdata.irr_area_ratio)))  # per area of non-irrigation
     return outdata
@@ -58,4 +58,4 @@ def gen_all_wy_averages(base_dir):
 
 
 if __name__ == '__main__':
-    gen_all_wy_averages(r"K:\niwa_netcdf\lsrm\lsrm_results")
+    gen_all_wy_averages(env.gw_met_data("niwa_netcdf/lsrm/lsrm_results"))
