034560293b89325f86faac8760e4d212ecc7f591
Mike Exner-Kittridge <Mike.Exner-Kittridge@ecan.govt.nz>
2017-09-29 14:06:26 +1300

    new bat files

---
+++ executables/windows/projects/freshwater_report/sw_precip_water_map.bat
@@ -0,0 +1,2 @@
+C:\Anaconda2_64bit\python.exe C:\git\Ecan.Science.Python.Base\users\MK\hydro\monthly_water_report\water_report_precip_sw_gw.py
+pause
--- executables/windows/projects/freshwater_report/test.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 29 13:59:42 2017
-
-@author: MichaelEK
-"""
-
