2d55335f9e249566732312c9fe0876e5ce2a9a34
MattH <hansonmcoombs@gmail.com>
2017-10-19 17:48:51 +1300

    build find string in scripts

--- users/MH/find_string_in_scripts.py
+++ users/MH/find_string_in_scripts.py
@@ -7,11 +7,10 @@ Date Created: 18/10/2017 11:29 AM
 from __future__ import division
 from core import env
 import os
+import pandas as pd
 
 # a scipt to test whether there are any overlaps... looking for duplicates
-
-if __name__ == '__main__':
-    directory = r'C:\Users\MattH\OneDrive - Environment Canterbury\Ecan_code4\Ecan.Science.Python.Base\users\MH\Waimak_modeling\models\extended_boundry'
+def find_str_in_scripts(directory, string):
     all_paths = []
     for root, dirs, files in os.walk(directory):
         for file in files:
@@ -24,9 +23,13 @@ if __name__ == '__main__':
         with open(path) as f:
             lines = f.readlines()
         for line in lines:
-            if 'pickle_path' in line and '=' in line:
+            if string in line:
                 scripts.append(script)
                 pickle_paths.append(line)
+    data = pd.DataFrame({'lines':pickle_paths,'scripts':scripts})
+    data.to_csv(os.path.join(env.temp("temp_gw_files"),'find_string_in_scripts.csv'))
 
-    print 'done'
 
+
+if __name__ == '__main__':
+    find_str_in_scripts() #todo if used
