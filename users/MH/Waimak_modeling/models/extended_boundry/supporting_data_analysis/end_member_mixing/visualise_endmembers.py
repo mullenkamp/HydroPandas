c4897895fa2abe9b5ea75c389f2603dcad44b64e
MattH <hansonmcoombs@gmail.com>
2017-11-10 19:44:08 +1300

    plot_endmember pdfs

--- users/MH/Waimak_modeling/models/extended_boundry/supporting_data_analysis/end_member_mixing/visualise_endmembers.py
+++ users/MH/Waimak_modeling/models/extended_boundry/supporting_data_analysis/end_member_mixing/visualise_endmembers.py
@@ -8,37 +8,80 @@ from __future__ import division
 from core import env
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 if __name__ == '__main__':
-    end_mean_o18 = {'inland': -8.76, 'coastal': -8.00, 'river': -9.25, 'eyre': -8.90}
-    end_sd_o18 = {'inland': 0.21, 'coastal': 0.64, 'river': 0.22, 'eyre': 0.54}
+    tracers = [
+        'cl',
+        'o18',
+        'k'
+    ]
+    endmembers = [
+        'inland',
+        'coastal',
+        'river',
+        'eyre'
+    ]
 
-    end_mean_cl = {'inland': 10.38, 'coastal': 25.43, 'river': 1.05, 'eyre': 3.67}
-    end_sd_cl = {'inland': 4.21, 'coastal': 9.49, 'river': 0.15, 'eyre': 0.29}
-    o18s = {}
-    cls = {}
-    for site in end_mean_cl.keys():
-        cls[site] = np.random.normal(end_mean_cl[site], end_sd_cl[site], 100000)
-        o18s[site] = np.random.normal(end_mean_o18[site], end_sd_o18[site], 100000)
+    means = {
+        'cl': {
+            'inland': 10.38,
+            'coastal': 25.43,
+            'river': 1.05,
+            'eyre': 3.67
+        },
+        'o18': {
+            'inland': -8.76,
+            'coastal': -8.00,
+            'river': -9.25,
+            'eyre': -8.90
+        },
+        'k': {
+            'inland': 1.23,
+            'coastal': 1.67,
+            'river': 0.57,
+            'eyre': 1.17
+        }
+    }
 
-    # o18
-    fig, ax = plt.subplots(figsize=(18.5, 9.5))
-    ax.hist(o18s['coastal'], bins=101, color='orange', label='coastal', alpha=0.5)
-    ax.hist(o18s['inland'], bins=101, color='r', label='inland', alpha=0.5)
-    ax.hist(o18s['river'], bins=101, color='b', label='alpine_river', alpha=0.5)
-    ax.hist(o18s['eyre'], bins=101, color='g', label='eyre', alpha=0.5)
-    ax.set_title('o18')
-    ax.legend()
+    sds = {
+        'cl': {
+            'inland': 4.21,
+            'coastal': 9.49,
+            'river': 0.15,
+            'eyre': 0.29
+        },
+        'o18': {
+            'inland': 0.21,
+            'coastal': 0.64,
+            'river': 0.22,
+            'eyre': 0.54
+        },
+        'k': {
+            'inland': 0.31,
+            'coastal': 0.30,
+            'river': 0.15,
+            'eyre': 0.12
+        }
+    }
 
-    # cl
-    fig2, ax2 = plt.subplots(figsize=(18.5, 9.5))
-    ax2.hist(cls['coastal'], bins=101, color='orange', label='coastal', alpha=0.5)
-    ax2.hist(cls['inland'], bins=101, color='r', label='inland', alpha=0.5)
-    ax2.hist(cls['river'], bins=101, color='b', label='alpine_river', alpha=0.5)
-    ax2.hist(cls['eyre'], bins=101, color='g', label='eyre', alpha=0.5)
-    ax2.set_title('cl')
-    ax2.legend()
+    a_errors = {
+        'cl': 0.5,
+        'o18': 0.2,
+        'k': 0.15
+    }
 
-    fig3, ax3 = plt.subplots(figsize=(18.5, 9.5))
-    ax3.boxplot([cls['coastal'], cls['inland'], cls['river'], cls['eyre']])
-    plt.show()
+    outdir =r"\\gisdata\projects\SCI\Groundwater\Waimakariri\Groundwater\Groundwater Quality\End member mixing model\Additional target wells\4_members_plots"
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    for trace in tracers:
+        fig, ax = plt.subplots(figsize=(18.5, 9.5))
+        for em in endmembers:
+            data = np.random.normal(means[trace][em], sds[trace][em] + a_errors[trace], 100000)
+            ax.hist(data, bins=101, label=em, alpha=0.5)
+
+        fig.suptitle('tracer: {}'.format(trace))
+        ax.legend()
+        fig.savefig(os.path.join(outdir, '{}_endmembers.png'.format(trace)))
+
+
