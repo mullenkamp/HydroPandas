fb0f51c4c4adc2cd7f240210a4d1519729363a0e
matth <matt.hanson@ecan.govt.nz>
2017-11-16 15:31:56 +1300

    debugging of the 2d plot

--- users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/nsmc_visualisation/observations/obs_2d_mean_sd.py
+++ users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/nsmc_visualisation/observations/obs_2d_mean_sd.py
@@ -37,7 +37,7 @@ def neg_zero_pos(x, **kwargs):
 
 def plot_sd_mean_multid(filter_strs, layer, nc_param_data, nc_obs_data, data_id, function_adjust,
                         title, basemap=True, contour={'sd': True, 'mean': True, 'sum': True}, method='mean_sd',
-                        contour_color='g'):
+                        contour_color='g', vmins=None, vmaxes=None):
     filter_strs = np.atleast_1d(filter_strs)
     param_nc_nums = np.array(nc_param_data.variables['nsmc_num'])
     obs_nc_nums = np.array(nc_obs_data.variables['nsmc_num'])
@@ -92,21 +92,19 @@ def plot_sd_mean_multid(filter_strs, layer, nc_param_data, nc_obs_data, data_id,
         real_filters = np.where(real_filters)[0]
         # pull the data out
         if nc_obs_data.variables[data_id].ndim == 4:
-            print 'starting to pull data'
-            import time
-            t= time.time()
             temp = nc_obs_data.variables[data_id][real_filters, layer, :, :]
-            print 'pull_masked {}'.format((time.time()-t)/60)
-            temp = function_adjust(np.array(nc_obs_data.variables[data_id][real_filters, layer]), layer=layer)
-            print 'pull_unmasked {}'.format((time.time()-t)/60)
-            print 'data pulled'
-            raise
+            if isinstance(temp,np.ma.MaskedArray):
+                temp = temp.filled(np.nan)
+            temp = function_adjust(temp, layer=layer)
         elif nc_obs_data.variables[data_id].ndim == 3:
-            temp = function_adjust(np.array(nc_obs_data.variables[data_id][real_filters]), layer=0)
+            temp = nc_obs_data.variables[data_id][real_filters]
+            if isinstance(temp,np.ma.MaskedArray):
+                temp = temp.filled(np.nan)
+            temp = function_adjust(temp, layer=0)
         else:
             raise ValueError('{} does not have 3 or 4 dims'.format(data_id))
 
-        mean = np.nanmean(temp, axis=0)
+        mean = np.nanmean(temp, axis=0) #todo check this
         sd = np.nanstd(temp, axis=0)
         dsum = np.nansum(temp, axis=0)
         plot_data_mean[filter_str] = mean
@@ -121,20 +119,52 @@ def plot_sd_mean_multid(filter_strs, layer, nc_param_data, nc_obs_data, data_id,
 
     # initalised teh figure
     if method == 'mean_sd':
-        nrows = 2
+        ncols = 2
     elif method == 'sum':
-        nrows = 1
+        ncols = 1
     else:
         raise ValueError('unexpected value for method')
-    fig, axs = plt.subplots(nrows=nrows, ncols=len(filter_strs), figsize=(18.5, 9.5))
+    fig, axs = plt.subplots(ncols=ncols, nrows=len(filter_strs), figsize=(18.5, 9.5))
     axs = np.atleast_2d(axs)
-
-    mean_vmax = max(mean_maxes)
     mean_vmin = min(mean_mins)
-    sd_vmax = max(sd_maxes)
     sd_vmin = min(sd_mins)
     sum_vmin = min(sum_mins)
+    if vmins is not None:
+        if not isinstance(vmins,dict):
+            raise ValueError('vmins must be none or dict')
+        try:
+           mean_vmin = vmins['mean']
+        except KeyError:
+            pass
+        try:
+           sd_vmin = vmins['sd']
+        except KeyError:
+            pass
+        try:
+           sum_vmin = vmins['sum']
+        except KeyError:
+            pass
+
+    mean_vmax = max(mean_maxes)
+    sd_vmax = max(sd_maxes)
     sum_vmax = max(sum_maxes)
+
+    if vmaxes is not None:
+        if not isinstance(vmaxes,dict):
+            raise ValueError('vmaxes must be none or dict')
+
+        try:
+           mean_vmax = vmaxes['mean']
+        except KeyError:
+            pass
+        try:
+           sd_vmax = vmaxes['sd']
+        except KeyError:
+            pass
+        try:
+           sum_vmax = vmaxes['sum']
+        except KeyError:
+            pass
 
     for i, fstr in enumerate(use_filter_strs):
         plt_cbar = False
@@ -148,13 +178,13 @@ def plot_sd_mean_multid(filter_strs, layer, nc_param_data, nc_obs_data, data_id,
             cbar_lab_sum = 'Sum'
 
         if method == 'mean_sd':
-            meanax = axs[0, i]
-            sdax = axs[1, i]
-            # todo contour the data (what color), include basemap?
+            meanax = axs[i, 0]
+            sdax = axs[i, 1]
             smt.plt_matrix(plot_data_mean[fstr], vmin=mean_vmin, vmax=mean_vmax,
                            title='{} mean'.format(fstr),
-                           no_flow_layer=0, ax=meanax, color_bar=plt_cbar, base_map=basemap,
-                           cbar_lab=cbar_lab_mean)
+                           no_flow_layer=layer, ax=meanax, color_bar=plt_cbar, base_map=basemap,
+                           cbar_lab=cbar_lab_mean,
+                           )
 
             if contour['mean']:
                 x, y = smt.get_model_x_y()
@@ -162,27 +192,31 @@ def plot_sd_mean_multid(filter_strs, layer, nc_param_data, nc_obs_data, data_id,
 
             smt.plt_matrix(plot_data_sd[fstr], vmin=sd_vmin, vmax=sd_vmax,
                            title='{} sd'.format(fstr),
-                           no_flow_layer=0, ax=sdax, color_bar=plt_cbar, base_map=basemap,
-                           cbar_lab=cbar_lab_sd)
+                           no_flow_layer=layer, ax=sdax, color_bar=plt_cbar, base_map=basemap,
+                           cbar_lab=cbar_lab_sd,
+                           )
             if contour['sd']:
                 x, y = smt.get_model_x_y()
                 sdax.contour(x, y, plot_data_sd[fstr], colors=contour_color)
-
+            meanax.set_title('Mean {}'.format(fstr))
+            sdax.set_title('Std {}'.format(fstr))
         elif method == 'sum':
             sumax = axs[0, i]
             smt.plt_matrix(plot_data_sum[fstr], vmin=sum_vmin, vmax=sum_vmax,
                            title='{} sum'.format(fstr),
-                           no_flow_layer=0, ax=sumax, color_bar=plt_cbar, base_map=basemap,
-                           cbar_lab=cbar_lab_sum)
+                           no_flow_layer=layer, ax=sumax, color_bar=plt_cbar, base_map=basemap,
+                           cbar_lab=cbar_lab_sum,
+                           )
 
             if contour['sum']:
                 x, y = smt.get_model_x_y()
                 sumax.contour(x, y, plot_data_sum[fstr], colors=contour_color)
-
+            sumax.set_title('sum {}'.format(fstr))
         else:
             raise ValueError("shouldn't get here")
 
     fig.suptitle(title.title())
+    fig.tight_layout()
     return fig, axs
 
 
@@ -199,52 +233,57 @@ def plot_all_2d_obs(outdir,filter_strs):
 
     # hds by layer
     for l in range(smt.layers):
-        title = 'heads for layer: {:02d}'.format(l)
+        title = 'heads for layer {:02d}'.format(l+1)
+        print('plotting {}'.format(title))
         fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
                                        nc_param_data=nc_param_data, nc_obs_data=nc_hds_data,
                                        data_id='heads', function_adjust=no_change,
                                        title=title,
-                                       basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
+                                       basemap=True, contour={'sd':False, 'mean': True, 'sum': True},
                                        method='mean_sd', contour_color='g')
 
-        fig.savefig(os.path.join(outdir, title))
+
+        fig.savefig(os.path.join(outdir, title.replace(' ','_')+'.png'))
         plt.close()
 
     # drn flux
     title = 'drain flux'
+    print('plotting {}'.format(title))
     fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=0,
                                    nc_param_data=nc_param_data, nc_obs_data=nc_bud_data,
                                    data_id='drains', function_adjust=no_change,
                                    title=title,
-                                   basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
+                                   basemap=True, contour={'sd': False, 'mean': True, 'sum': True},
                                    method='mean_sd', contour_color='g')
 
-    fig.savefig(os.path.join(outdir, title))
+    fig.savefig(os.path.join(outdir, title.replace(' ','_')+'.png'))
     plt.close()
 
     # layer one bottom flux (as 1 0 -1)
     title = 'layer 1 up vs downflow'
+    print('plotting {}'.format(title))
     fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=0,
                                    nc_param_data=nc_param_data, nc_obs_data=nc_bud_data,
                                    data_id='FLOW LOWER FACE'.lower(), function_adjust=neg_zero_pos,
                                    title=title,
-                                   basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
+                                   basemap=True, contour={'sd': False, 'mean': False, 'sum': True},
                                    method='mean_sd', contour_color='g')
 
-    fig.savefig(os.path.join(outdir, title))
+    fig.savefig(os.path.join(outdir, title.replace(' ','_')+'.png'))
     plt.close()
 
     # where the model is dry
     for l in range(smt.layers):
-        title = 'drycells for layer: {:02d}'.format(l)
+        title = 'drycells for layer {:02d}'.format(l+1)
+        print('plotting {}'.format(title))
         fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
                                        nc_param_data=nc_param_data, nc_obs_data=nc_hds_data,
                                        data_id='heads', function_adjust=is_dry,
                                        title=title,
-                                       basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
+                                       basemap=True, contour={'sd': False, 'mean': False, 'sum': True},
                                        method='mean_sd', contour_color='g')
 
-        fig.savefig(os.path.join(outdir, title))
+        fig.savefig(os.path.join(outdir, title.replace(' ','_')+'.png'))
         plt.close()
 
 
--- users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/nsmc_visualisation/observations/plot_ucn.py
+++ users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/nsmc_visualisation/observations/plot_ucn.py
@@ -15,22 +15,28 @@ from users.MH.Waimak_modeling.models.extended_boundry.extended_boundry_model_too
 import matplotlib.pyplot as plt
 import pandas as pd
 from warnings import warn
-from users.MH.Waimak_modeling.models.extended_boundry.supporting_data_analysis.all_well_layer_col_row import get_all_well_row_col
+from users.MH.Waimak_modeling.models.extended_boundry.supporting_data_analysis.all_well_layer_col_row import \
+    get_all_well_row_col
+
 
 def _above_mav(x, **kwargs):
     return x >= 11.3
 
+
 def _above_half_mav(x, **kwargs):
     return x >= 5.65
+
 
 def _above_quarter_mav(x, **kwargs):
     return x >= 2.825
 
+
 def _above_low(x, **kwargs):
     return x >= 1
 
+
 # todo decide on filters
-def plot_all_2d_con(outdir, filter_strs):
+def plot_all_2d_con(outdir, filter_strs): #todo set vmaxs and vmins
     if socket.gethostname() != 'GWATER02':
         raise ValueError('this must be run on GWATER02 as that is where the uncompressed data is stored')
 
@@ -40,85 +46,106 @@ def plot_all_2d_con(outdir, filter_strs):
 
     if not os.path.exists(outdir):
         os.makedirs(outdir)
+
     for l in range(smt.layers):
-        title = 'coastal component for layer: {:02d}'.format(l)
+        title = 'best N estimate for layer {:02d}'.format(l)
+        print(title)
+        fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
+                                       nc_param_data=nc_param_data, nc_obs_data=bestn,
+                                       data_id='mednload', function_adjust=no_change,
+                                       title=title,
+                                       basemap=True, contour={'sd': False, 'mean': False, 'sum': True},
+                                       method='mean_sd', contour_color='g')
+
+        fig.savefig(os.path.join(outdir, title.replace(' ', '_')))
+        plt.close()
+
+    for l in range(smt.layers):
+        title = 'best N estimate for layer {:02d} low scale'.format(l)
+        print(title)
+        fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
+                                       nc_param_data=nc_param_data, nc_obs_data=bestn,
+                                       data_id='mednload', function_adjust=no_change,
+                                       title=title,
+                                       basemap=True, contour={'sd': False, 'mean': False, 'sum': True},
+                                       method='mean_sd', contour_color='g',
+                                       vmaxes={'mean':11.3, 'sd':3}, vmins={'mean':1})
+
+        fig.savefig(os.path.join(outdir, title.replace(' ', '_')))
+        plt.close()
+
+    for l in range(smt.layers):
+        title = 'coastal component for layer {:02d}'.format(l)
+        print(title)
         fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
                                        nc_param_data=nc_param_data, nc_obs_data=emma,
                                        data_id='coastal', function_adjust=no_change,
                                        title=title,
-                                       basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
+                                       basemap=True, contour={'sd': False, 'mean': False, 'sum': True},
                                        method='mean_sd', contour_color='g')
 
-        fig.savefig(os.path.join(outdir, title))
+        fig.savefig(os.path.join(outdir, title.replace(' ', '_')))
         plt.close()
 
     for l in range(smt.layers):
-        title = 'inland component for layer: {:02d}'.format(l)
+        title = 'inland component for layer {:02d}'.format(l)
+        print(title)
         fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
                                        nc_param_data=nc_param_data, nc_obs_data=emma,
                                        data_id='inland', function_adjust=no_change,
                                        title=title,
-                                       basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
+                                       basemap=True, contour={'sd': False, 'mean': False, 'sum': True},
                                        method='mean_sd', contour_color='g')
 
-        fig.savefig(os.path.join(outdir, title))
+        fig.savefig(os.path.join(outdir, title.replace(' ', '_')))
         plt.close()
 
     for l in range(smt.layers):
-        title = 'river component for layer: {:02d}'.format(l)
+        title = 'river component for layer {:02d}'.format(l)
+        print(title)
         fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
                                        nc_param_data=nc_param_data, nc_obs_data=emma,
                                        data_id='river', function_adjust=no_change,
                                        title=title,
-                                       basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
+                                       basemap=True, contour={'sd': False, 'mean': False, 'sum': True},
                                        method='mean_sd', contour_color='g')
 
-        fig.savefig(os.path.join(outdir, title))
+        fig.savefig(os.path.join(outdir, title.replace(' ', '_')))
         plt.close()
 
-    for l in range(smt.layers):
-        title = 'best N estimate (g/m3) for layer: {:02d}'.format(l)
-        fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
-                                       nc_param_data=nc_param_data, nc_obs_data=bestn,
-                                       data_id='inland', function_adjust=no_change,
-                                       title=title,
-                                       basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
-                                       method='mean_sd', contour_color='g')
-
-        fig.savefig(os.path.join(outdir, title))
-        plt.close()
-
-    for limit, ftn in zip([1, 2.85, 5.65, 11.3], [_above_low,_above_quarter_mav, _above_half_mav, _above_mav]):
+    for limit, ftn in zip([1, 2.85, 5.65, 11.3], [_above_low, _above_quarter_mav, _above_half_mav, _above_mav]):
         for l in range(smt.layers):
-            title = 'N above {} (g/m3) for layer: {:02d}'.format(limit, l)
+            title = 'N above {} (gm3) for layer {:02d}'.format(limit, l)
+            print(title)
             fig, axs = plot_sd_mean_multid(filter_strs=filter_strs, layer=l,
                                            nc_param_data=nc_param_data, nc_obs_data=bestn,
-                                           data_id='inland', function_adjust=ftn,
+                                           data_id='mednload', function_adjust=ftn,
                                            title=title,
-                                           basemap=True, contour={'sd': True, 'mean': True, 'sum': True},
+                                           basemap=True, contour={'sd': True, 'mean': False, 'sum': True},
                                            method='mean_sd', contour_color='g')
 
-            fig.savefig(os.path.join(outdir, title))
+            fig.savefig(os.path.join(outdir, title.replace(' ', '_')))
             plt.close()
 
 
 # both 2D spatial as well as point measurements at wells for example
 # take for granted that only 2000 something were run when looking through filters
 
-def plot_well_con(nc_path, outdir, filter_strs): #todo debug
+def plot_well_con(param_nc_path, con_nc_path, con_str, outdir, filter_strs):  # todo debug
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    plt_data = pd.read_excel(r"\\gisdata\projects\SCI\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model build and optimisation\WellNSMCPlotNitrateGroups.xlsx")
+    plt_data = pd.read_excel(
+        r"\\gisdata\projects\SCI\Groundwater\Waimakariri\Groundwater\Numerical GW model\Model build and optimisation\WellNSMCPlotNitrateGroups.xlsx")
     all_wells = get_all_well_row_col()
     filter_strs = np.atleast_1d(filter_strs)
-    nc_data = nc.Dataset(nc_path)
+    con_nc_data = nc.Dataset(con_nc_path)
+    param_nc_data = nc.Dataset(param_nc_path)
     supergroups = list(set(plt_data['Supergroup']))
 
     for supergroup in supergroups:
         fig, axs = plt.subplots(ncols=len(filter_strs), figsize=(18.5, 9.5))
         axs = np.atleast_1d(axs)
         plot_sites = list(plt_data.loc[plt_data['Supergroup'] == supergroup, 'Group'].values)
-        well_names = np.array(nc_data.variables['well_name'])
         for filter_str_raw, ax in zip(filter_strs, axs):
             ftype = 0
             filter_str = filter_str_raw
@@ -136,7 +163,7 @@ def plot_well_con(nc_path, outdir, filter_strs): #todo debug
                 ftype = 3
                 textadd = 'not run or failed '
 
-            temp_filter = np.array(nc_data.variables[filter_str])
+            temp_filter = np.array(param_nc_data.variables[filter_str])
             if ftype == 0:
                 real_filter = temp_filter == 1
             elif ftype == 1:
@@ -148,28 +175,36 @@ def plot_well_con(nc_path, outdir, filter_strs): #todo debug
             else:
                 raise ValueError('shouldnt get here')
 
-            nsmc_nums = np.array(nc_data.variables['nsmc_num'][real_filter])
+            nsmc_nums_param = np.array(
+                param_nc_data.variables['nsmc_num'][real_filter])  # todo make this work between filters
+
+            nsmc_nums_con = np.array(con_nc_data.variables['nsmc_num'])
+            real_filter = np.in1d(nsmc_nums_con, nsmc_nums_param)
             all_model_data = []
 
             # get the data
             labels = []
-            for site in plot_sites:
-                layer, row, col = all_wells.loc[plt_data.loc[plt_data['Group']==site,'WELL_NO'].values,['layer','row','col']].values.transpose()
-                if len(layer) == 0 :
+            for site in set(plot_sites):
+                print(site)
+                layer, row, col = all_wells.loc[
+                    plt_data.loc[plt_data['Group'] == site, 'WELL_NO'].values, ['layer', 'row',
+                                                                                'col']].dropna().values.astype(
+                    int).transpose()
+                if len(layer) == 0:
                     warn('no wells for site {} not in observations'.format(site))
                     continue
                 labels.append(site)
 
-
-                model_data = np.array([nc_data.variables['well_obs'][real_filter, l, r, c] for l,r,c in zip(layer, row, col)])
-                model_data = model_data.sum(axis=0)
+                model_data = np.array(
+                    [con_nc_data.variables[con_str][real_filter, l, r, c] for l, r, c in zip(layer, row, col)])
+                model_data = model_data.mean(axis=0)
                 all_model_data.append(model_data[np.isfinite(model_data)])
 
             # plot it up
             positions = np.arange(len(labels)) + 1
             t = ax.boxplot(x=all_model_data, positions=positions, labels=labels)
             [[e.set_linewidth(2) for e in j[1]] for j in t.items()]
-            ax.set_ylabel('m')
+            ax.set_ylabel('mg/l')
             ax.set_title('{}{}'.format(textadd, filter_str))
         ymax = max([e.get_ylim()[1] for e in axs.flatten()])
         ymin = min([e.get_ylim()[0] for e in axs.flatten()])
@@ -179,3 +214,12 @@ def plot_well_con(nc_path, outdir, filter_strs): #todo debug
         fig.suptitle(supergroup.title())
 
         fig.savefig(os.path.join(outdir, supergroup.lower()))
+
+
+if __name__ == '__main__':
+    plot_well_con(r"K:\mh_modeling\netcdfs_of_key_modeling_data\nsmc_params_obs_metadata.nc",
+                  r"C:\mh_waimak_model_data\mednload_ucn.nc",
+                  con_str='mednload',
+                  outdir=r"T:\Temp\temp_gw_files\testcon1dplots",
+                  filter_strs=['filter2', 'filter3'])
+    plot_all_2d_con(r"T:\Temp\temp_gw_files\testcon2dplots", filter_strs=['filter2', 'filter3'])
