2a5c16aaae250ee031f8f8da69cbf4f700449df6
MattH <hansonmcoombs@gmail.com>
2017-12-08 13:02:34 +1300

    try to run emulator

--- users/MH/scratch.py
+++ users/MH/scratch.py
@@ -1,9 +1,14 @@
 from __future__ import division
 from users.MH.Waimak_modeling.models.extended_boundry.m_packages import create_wel_package
 from users.MH.Waimak_modeling.models.extended_boundry.m_packages.wel_packages import create_wel_package
+from users.MH.Waimak_modeling.models.extended_boundry.extended_boundry_model_tools import smt
 
 import netCDF4 as nc
 import numpy as np
+
+def timeit_test():
+    layers, rows, cols = np.meshgrid(range(smt.layers), range(smt.rows), range(smt.cols), indexing='ij')
+    ids = ['{:02d}_{:03d}_{:03d}'.format(k, i, j) for k, i, j in zip(layers.flatten(), rows.flatten(), cols.flatten())]
 
 if __name__ == '__main__':
     data = nc.Dataset(r"C:\Users\MattH\Downloads\test.nc",'w')
