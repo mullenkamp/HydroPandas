3cbb6b66843e08c0b3359472c80263e09aeb5277
matth <matt.hanson@ecan.govt.nz>
2017-12-18 16:33:38 +1300

    work with ashley losses more

--- users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/upper_ashley_losses.py
+++ users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/upper_ashley_losses.py
@@ -9,13 +9,29 @@ from core import env
 import netCDF4 as nc
 from users.MH.Waimak_modeling.models.extended_boundry.extended_boundry_model_tools import smt
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-if __name__ == '__main__':
-    mask = smt.shape_file_to_model_array(r"{}\m_ex_bd_inputs\raw_sw_samp_points\sfr\other\ashley_brodricks.shp".format(smt.sdp),'k',True)
+if __name__ == '__main__': #todo check waimak_con vs ashley losses
+    mask = smt.shape_file_to_model_array(r"{}\m_ex_bd_inputs\raw_sw_samp_points\sfr\other\ashley_garry.shp".format(smt.sdp),'k',True)
     mask = np.isfinite(mask)
     idxs = smt.model_where(mask)
     data = nc.Dataset(r"C:\mh_waimak_model_data\post_filter1_budget.nc")
 
+    outdata = [data.variables['stream leakage'][:,e[0],e[1]][:,np.newaxis] for e in idxs]
+    outdata = np.concatenate(outdata,axis=1).sum(axis=1)/86400
+    outdata = pd.Series(outdata,index=data.variables['nsmc_num'][:])
 
-    'stream leakage'
+    cons = nc.Dataset(r"C:\mh_waimak_model_data\mednload_ucn.nc")
+    chch_n_average = smt.shape_file_to_model_array(r"{}\m_ex_bd_inputs\shp\rough_chch.shp".format(smt.sdp),'Id',True)
+    mask = np.isfinite(chch_n_average)
+    idxs = smt.model_where(mask)
+    out_chch_n_average = np.concatenate([np.array(cons.variables['mednload'][:, :, e[0], e[1]])[:,:,np.newaxis] for e in idxs],axis=2) #todo check dim
+    out_chch_n_average = np.nanmean(out_chch_n_average,axis=2)
+    out_chch_n_average = np.nanmean(out_chch_n_average,axis=1)
+    out_chch_n_average = pd.Series(out_chch_n_average,index=cons.variables['nsmc_num'[:]])
+
+    out = pd.merge(outdata.to_frame('ash'), out_chch_n_average.to_frame('con'),right_index=True,left_index=True)
+    out.to_csv(r"C:\Users\matth\Downloads\ashley_and_chch.csv")
+
 
