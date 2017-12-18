ea34f0c0d49ee253278a4b9c1eb13bf3a471cc55
matth <matt.hanson@ecan.govt.nz>
2017-12-19 09:42:06 +1300

    work with ashley losses more

--- users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/upper_ashley_losses.py
+++ users/MH/Waimak_modeling/models/extended_boundry/nsmc_exploration_results/upper_ashley_losses.py
@@ -22,6 +22,14 @@ if __name__ == '__main__': #todo check waimak_con vs ashley losses
     outdata = np.concatenate(outdata,axis=1).sum(axis=1)/86400
     outdata = pd.Series(outdata,index=data.variables['nsmc_num'][:])
 
+    mask = smt.shape_file_to_model_array(r"{}\m_ex_bd_inputs\raw_sw_samp_points\sfr\other\cust_benit_cust.shp".format(smt.sdp),'k',True)
+    mask = np.isfinite(mask)
+    idxs = smt.model_where(mask)
+
+    outdata_cust = [data.variables['stream leakage'][:,e[0],e[1]][:,np.newaxis] for e in idxs]
+    outdata_cust = np.concatenate(outdata_cust,axis=1).sum(axis=1)/86400
+    outdata_cust = pd.Series(outdata_cust,index=data.variables['nsmc_num'][:])
+
     cons = nc.Dataset(r"C:\mh_waimak_model_data\mednload_ucn.nc")
     chch_n_average = smt.shape_file_to_model_array(r"{}\m_ex_bd_inputs\shp\rough_chch.shp".format(smt.sdp),'Id',True)
     mask = np.isfinite(chch_n_average)
@@ -32,6 +40,7 @@ if __name__ == '__main__': #todo check waimak_con vs ashley losses
     out_chch_n_average = pd.Series(out_chch_n_average,index=cons.variables['nsmc_num'[:]])
 
     out = pd.merge(outdata.to_frame('ash'), out_chch_n_average.to_frame('con'),right_index=True,left_index=True)
+    out = pd.merge(out,outdata_cust.to_frame('cust'),right_index=True,left_index=True)
     out.to_csv(r"C:\Users\matth\Downloads\ashley_and_chch.csv")
 
 
