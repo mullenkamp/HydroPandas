73ac9a27ef341bff66cc942ebc6229f8b903dac6
MikeK <mgkittridge@gmail.com>
2018-01-16 22:07:36 +1300

    requests

--- users/MK/hydro/low_flow/lowflow_sites_cwms_catch_grp.py
+++ users/MK/hydro/low_flow/lowflow_sites_cwms_catch_grp.py
@@ -5,6 +5,7 @@ Created on Fri Jan 12 08:35:28 2018
 @author: MichaelEK
 """
 import pandas as pd
+import geopandas as gpd
 from hydropandas.io.tools.sql_arg_class import sql_arg
 from hydropandas.io.tools.mssql import to_mssql, create_mssql_table, rd_sql
 from hydropandas.tools.general.spatial.vector import pts_poly_join
@@ -13,20 +14,42 @@ from hydropandas.tools.general.spatial.vector import pts_poly_join
 ### Parameters
 ## Import
 sql_lowflow_site = 'lowflow_sites_gis'
+sql_residual_site = 'residual_sites_gis'
 sql_cwms = 'cwms_gis'
 sql_catch_grp = 'catch_grp_gis'
+sql_site_restr = {'server': 'SQL2012DEV01', 'database': 'Hydro', 'table': 'LowFlowRestrSite'}
 
 ## Export
 sql_site_dict = {'server': 'SQL2012DEV01', 'database': 'Hydro', 'table': 'LowFlowSiteGeo1'}
-site_dtype_dict = {'SiteID': 'int', 'site': 'varchar(19)', 'cwms': 'varchar(49)', 'catch_grp': 'int', 'catch_grp_name': 'varchar(49)'}
+site_dtype_dict = {'SiteID': 'int', 'site': 'varchar(19)', 'site_type': 'varchar(9)', 'cwms': 'varchar(49)', 'catch_grp': 'int', 'catch_grp_name': 'varchar(49)', 'flow_method': 'varchar(59)', 'crc_count': 'int'}
 site_pkey = ['site']
+
+sql_cwms_dict = {'server': 'SQL2012DEV01', 'database': 'Hydro', 'table': 'LowFlowSiteCWMS'}
+cwms_dtype_dict = {'cwms': 'varchar(59)', 'lowflow_tot': 'int', 'residual_tot': 'int', 'lowflow_crc': 'int', 'residual_crc': 'int', 'lowflow_telem': 'int', 'residual_telem': 'int'}
+cwms_pkey = 'cwms'
+
+sql_catch_dict = {'server': 'SQL2012DEV01', 'database': 'Hydro', 'table': 'LowFlowSiteCatch'}
+catch_dtype_dict = {'catch_grp': 'int', 'lowflow_tot': 'int', 'residual_tot': 'int', 'lowflow_crc': 'int', 'residual_crc': 'int', 'lowflow_telem': 'int', 'residual_telem': 'int'}
+catch_pkey = 'catch_grp'
 
 ################################
 ### Read data and combine
 
+restr = rd_sql(where_col={'date': ['2018-01-15']}, **sql_site_restr)
+restr1 = restr[['site', 'site_type', 'flow_method', 'crc_count']].copy()
+
 sql1 = sql_arg()
 
-sites = rd_sql(**sql1.get_dict(sql_lowflow_site))
+lowflow_dict = sql1.get_dict(sql_lowflow_site)
+lowflow_dict
+
+lowflow_sites = rd_sql(**sql1.get_dict(sql_lowflow_site))
+#lowflow_sites['site_type'] = 'lowflow'
+residual_sites = rd_sql(**sql1.get_dict(sql_residual_site))
+#residual_sites['site_type'] = 'residual'
+
+sites = pd.concat([lowflow_sites, residual_sites])
+
 cwms = rd_sql(**sql1.get_dict(sql_cwms))
 catch_grp = rd_sql(**sql1.get_dict(sql_catch_grp))
 
@@ -36,10 +59,61 @@ sites_cwms_catch, poly2 = pts_poly_join(sites_cwms, catch_grp, ['catch_grp', 'ca
 
 sites_cwms_catch1 = pd.DataFrame(sites_cwms_catch.drop('geometry', axis=1).copy())
 
+set1 = pd.merge(sites_cwms_catch1, restr1, on='site')
+
+## Summarize
+# CWMS
+cwms_grp = set1.groupby('cwms')
+cwms_lowflow = cwms_grp['site_type'].apply(lambda x: (x == 'LowFlow').sum())
+cwms_residual = cwms_grp['site_type'].apply(lambda x: (x == 'Residual').sum())
+cwms_lowflow_crc = cwms_grp.apply(lambda x: x.loc[x.site_type == 'LowFlow', 'crc_count'].sum())
+cwms_residual_crc = cwms_grp.apply(lambda x: x.loc[x.site_type == 'Residual', 'crc_count'].sum())
+cwms_lowflow_telem = cwms_grp.apply(lambda x: x.loc[(x.site_type == 'LowFlow') & (x.flow_method == 'Telemetered'), 'flow_method'].count())
+cwms_residual_telem = cwms_grp.apply(lambda x: x.loc[(x.site_type == 'Residual') & (x.flow_method == 'Telemetered'), 'flow_method'].count())
+
+cwms_summ = pd.concat([cwms_lowflow, cwms_residual, cwms_lowflow_crc, cwms_residual_crc, cwms_lowflow_telem, cwms_residual_telem], axis=1)
+cwms_summ.columns = ['lowflow_tot', 'residual_tot', 'lowflow_crc', 'residual_crc', 'lowflow_telem', 'residual_telem']
+
+# catch grp
+catch_grp_grp = set1.groupby('catch_grp')
+catch_grp_lowflow = catch_grp_grp['site_type'].apply(lambda x: (x == 'LowFlow').sum())
+catch_grp_residual = catch_grp_grp['site_type'].apply(lambda x: (x == 'Residual').sum())
+catch_grp_lowflow_crc = catch_grp_grp.apply(lambda x: x.loc[x.site_type == 'LowFlow', 'crc_count'].sum())
+catch_grp_residual_crc = catch_grp_grp.apply(lambda x: x.loc[x.site_type == 'Residual', 'crc_count'].sum())
+catch_grp_lowflow_telem = catch_grp_grp.apply(lambda x: x.loc[(x.site_type == 'LowFlow') & (x.flow_method == 'Telemetered'), 'flow_method'].count())
+catch_grp_residual_telem = catch_grp_grp.apply(lambda x: x.loc[(x.site_type == 'Residual') & (x.flow_method == 'Telemetered'), 'flow_method'].count())
+
+catch_grp_summ = pd.concat([catch_grp_lowflow, catch_grp_residual, catch_grp_lowflow_crc, catch_grp_residual_crc, catch_grp_lowflow_telem, catch_grp_residual_telem], axis=1)
+catch_grp_summ.columns = ['lowflow_tot', 'residual_tot', 'lowflow_crc', 'residual_crc', 'lowflow_telem', 'residual_telem']
+
 ### Same to SQL
 create_mssql_table(primary_keys=site_pkey, dtype_dict=site_dtype_dict, **sql_site_dict)
+create_mssql_table(primary_keys=cwms_pkey, dtype_dict=cwms_dtype_dict, **sql_cwms_dict)
+create_mssql_table(primary_keys=catch_pkey, dtype_dict=catch_dtype_dict, **sql_catch_dict)
 
-to_mssql(sites_cwms_catch1, **sql_site_dict)
+to_mssql(set1, **sql_site_dict)
+to_mssql(cwms_summ, index=True, **sql_cwms_dict)
+to_mssql(catch_grp_summ, **sql_catch_dict, index=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- users/MK/hydro/low_flow/restr_sites_map_inputs.py
+++ users/MK/hydro/low_flow/restr_sites_map_inputs.py
@@ -5,25 +5,20 @@ Created on Tue Dec 05 09:34:22 2017
 @author: MichaelEK
 """
 import numpy as np
-import pandas as pd
 from datetime import date
 from core.allo_use.ros import low_flow_restr
-from hydropandas.io.tools.mssql import to_mssql, create_mssql_table
+from hydropandas.io.tools.mssql import to_mssql, create_mssql_table, rd_sql
 
 ############################################
 ### Parameters
 
 sql_site_dict = {'server': 'SQL2012DEV01', 'database': 'Hydro', 'table': 'LowFlowRestrSite'}
 sql_site_band_dict = {'server': 'SQL2012DEV01', 'database': 'Hydro', 'table': 'LowFlowRestrSiteBand'}
-site_dtype_dict = {'site': 'varchar(19)', 'date': 'date', 'Waterway': 'varchar(59)', 'Location': 'varchar(59)', 'flow_method': 'varchar(29)', 'days_since_flow_est': 'int', 'flow': 'numeric(10,3)', 'crc_count': 'int', 'min_trig': 'numeric(10,3)', 'max_trig': 'numeric(10,3)', 'restr_category': 'varchar(9)'}
-site_band_dtype_dict = {'site': 'varchar(19)', 'band_num': 'int', 'date': 'date', 'Waterway': 'varchar(59)', 'Location': 'varchar(59)', 'flow_method': 'varchar(29)', 'days_since_flow_est': 'int', 'flow': 'numeric(10,3)', 'crc_count': 'int', 'min_trig': 'numeric(10,3)', 'max_trig': 'numeric(10,3)', 'band_allo': 'int'}
+#site_dtype_dict = {'site': 'varchar(19)', 'date': 'date', 'waterway': 'varchar(59)', 'location': 'varchar(59)', 'site_type': 'varchar(9)', 'flow_method': 'varchar(29)', 'days_since_flow_est': 'int', 'flow': 'numeric(10,3)', 'crc_count': 'int', 'min_trig': 'numeric(10,3)', 'max_trig': 'numeric(10,3)', 'restr_category': 'varchar(9)'}
+#site_band_dtype_dict = {'site': 'varchar(19)', 'band_num': 'int', 'date': 'date', 'waterway': 'varchar(59)', 'location': 'varchar(59)', 'site_type': 'varchar(9)', 'flow_method': 'varchar(29)', 'days_since_flow_est': 'int', 'flow': 'numeric(10,3)', 'crc_count': 'int', 'min_trig': 'numeric(10,3)', 'max_trig': 'numeric(10,3)', 'band_allo': 'int'}
 
-site_pkey = ['site', 'date']
-site_band_pkey = ['site', 'band_num', 'date']
-
-#server = 'SQL2012DEV01'
-#database = 'Hydro'
-#table = 'LowFlowSiteRestr'
+#site_pkey = ['site', 'date']
+#site_band_pkey = ['site', 'band_num', 'date']
 
 ###########################################
 ### Run function
@@ -32,8 +27,9 @@ today1 = str(date.today())
 
 basic, complete = low_flow_restr(from_date=today1, to_date=today1, only_restr=False)
 
-#basic['restr_category'] = basic['restr_category'].astype('category')
-#basic1.apply(lambda x: x.astype(str).str.len().max())
+## Backfill if needed
+#basic, complete = low_flow_restr(from_date='2017-10-01', to_date='2018-01-14', only_restr=False)
+#basic.apply(lambda x: x.astype(str).str.len().max())
 #basic['days_since_flow_est'] = np.nan
 #complete['days_since_flow_est'] = np.nan
 
@@ -45,6 +41,7 @@ basic, complete = low_flow_restr(from_date=today1, to_date=today1, only_restr=Fa
 ## Save data
 to_mssql(basic, **sql_site_dict)
 to_mssql(complete, **sql_site_band_dict)
+
 
 
 
