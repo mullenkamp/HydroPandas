f93e951cf303a6143275f18bbd4d99ac8fb11b77
MikeK <mgkittridge@gmail.com>
2018-03-12 19:33:05 +1300

    updates

--- users/MK/hydro/low_flow/restr_sites_map_inputs.py
+++ users/MK/hydro/low_flow/restr_sites_map_inputs.py
@@ -6,7 +6,7 @@ Created on Tue Dec 05 09:34:22 2017
 """
 import numpy as np
 import pandas as pd
-from datetime import date
+from datetime import datetime
 from core.allo_use.ros import low_flow_restr
 from hydropandas.io.tools.mssql import to_mssql, create_mssql_table, rd_sql, del_mssql_table_rows
 
@@ -20,16 +20,23 @@ sql_site_band_dict = {'server': 'SQL2012DEV01', 'database': 'Hydro', 'table': 'L
 
 #site_pkey = ['site', 'date']
 #site_band_pkey = ['site', 'band_num', 'date']
+
+last_date_stmt = "select max(Time) from ExtractionLog where HydroTable='LowFlowRestrSite' and RunResult='pass'"
+
 sql_log = {'server': 'SQL2012DEV01', 'database': 'Hydro', 'table': 'ExtractionLog'}
 
 ###########################################
 ### Run function
-today1 = str(date.today())
+today = datetime.today()
+today1 = str(today.date())
 
 try:
+    print('Get last day of extraction')
+    last_date1 = rd_sql(stmt=last_date_stmt, **sql_log).loc[0][0].date()
+
     print('Querying LowFlows db')
 
-    basic, complete = low_flow_restr(from_date=today1, to_date=today1, only_restr=False)
+    basic, complete = low_flow_restr(from_date=str(last_date1), to_date=today1, only_restr=False)
 
     ## Backfill if needed
     #basic, complete = low_flow_restr(from_date='2017-10-01', to_date='2018-01-14', only_restr=False)
@@ -45,20 +52,24 @@ try:
     ## Save data
     print('Saving data')
 
-    del_mssql_table_rows(from_date=today1, to_date=today1, date_col='date', **sql_site_dict)
-    del_mssql_table_rows(from_date=today1, to_date=today1, date_col='date', **sql_site_band_dict)
+    del_mssql_table_rows(from_date=str(last_date1), to_date=today1, date_col='date', **sql_site_dict)
+    del_mssql_table_rows(from_date=str(last_date1), to_date=today1, date_col='date', **sql_site_band_dict)
 
     to_mssql(basic, **sql_site_dict)
     to_mssql(complete, **sql_site_band_dict)
 
     print('Success')
 
-    log1 = pd.DataFrame([[today1, sql_site_dict['table'], 'pass', 'all good', today1], [today1, sql_site_band_dict['table'], 'pass', 'all good', today1]], columns=['Time', 'HydroTable', 'RunResult', 'Comment', 'FromTime'])
+    today2 = str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    today3 = str(today.strftime('%Y-%m-%d %H:%M:%S'))
+
+    log1 = pd.DataFrame([[today3, sql_site_dict['table'], 'pass', 'all good', str(last_date1), today2], [today3, sql_site_band_dict['table'], 'pass', 'all good', str(last_date1), today2]], columns=['Time', 'HydroTable', 'RunResult', 'Comment', 'FromTime', 'RunTimeEnd'])
     to_mssql(log1, **sql_log)
 except Exception as err:
     err1 = err
     print(err1)
-    log2 = pd.DataFrame([[today1, sql_site_dict['table'], 'pass', str(err1), today1], [today1, sql_site_band_dict['table'], 'pass', str(err1), today1]], columns=['Time', 'HydroTable', 'RunResult', 'Comment', 'FromTime'])
+    today2 = str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    log2 = pd.DataFrame([[today3, sql_site_dict['table'], 'pass', str(err1), str(last_date1), today2], [today1, sql_site_band_dict['table'], 'pass', str(err1), str(last_date1), today2]], columns=['Time', 'HydroTable', 'RunResult', 'Comment', 'FromTime', 'RunTimeEnd'])
     to_mssql(log2, **sql_log)
 
 
